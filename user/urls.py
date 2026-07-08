004e7bd6b8e20312d74ee4b4caf2bc9989aa2663
Seyed Mohammad Soheil Sirousi <soheilsirousi@Soheil-Mac.local>
2026-07-08 13:15:46 +0330

    add profile edit to user nav bar

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import LoginView, RegisterView, ProfileView, LogoutView
+from user.views import LoginView, RegisterView, ProfileView, LogoutView, UserDetailView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('detail/', UserDetailView.as_view(), name='detail'),
 ]
--- user/views.py
+++ user/views.py
@@ -91,3 +91,44 @@ class ProfileView(LoginRequiredMixin, View):
         return render(request, self.template_name, context=data)
 
 
+class UserDetailView(LoginRequiredMixin, View):
+    template_name = 'detail.html'
+    login_url = '/user/login/'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get('email')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        new_password = request.POST.get('new_password')
+
+        user = request.user
+
+        if email is None or fname is None or lname is None:
+            response = {"error": "ایمیل، نام و نام خانوادگی الزامی می‌باشد.", "user": user}
+            return render(request, self.template_name, context=response)
+
+        if new_password is not None and new_password != '':
+            user.set_password(new_password)
+
+
+        pattern = r'^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$'
+        check = re.search(pattern, email)
+        if not check:
+            response = {"error": "ایمیل وارد شده معتبر نیست.", "user": user}
+            return render(request, self.template_name, context=response)
+
+        find_user = User.objects.filter(email=email)
+
+        if find_user.exists() and user != find_user[0]:
+            response = {"error": "این ایمیل قبلا ثبت شده است.", "user": user}
+            return render(request, self.template_name, context=response)
+
+        user.email = email
+        user.first_name = fname
+        user.last_name = lname
+        user.save()
+
+        return render(request, self.template_name)
